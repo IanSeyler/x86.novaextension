b81ae938e70c9bd2bf97d3b8aaa1603eae276320
Ian Seyler <ian@ians-macbook-air.local>
2022-10-29 19:28:19 +0000

    Add x86 registers and opcodes. Remove image.

--- test.asm
+++ test.asm
@@ -1,48 +1,18 @@
-@	test.s
-@	dan-c-underwood
-@	Version - 1.3.0
-@	07/06/20
-@
-@	Description - Example Arm Source File
+; test.asm
+; Ian Seyler
+; Version - 1.0
+; 2022-10-29
+; Description - Example x86 Source File
 
-strings:
-string_a: 	defw	"Awaiting input"
-string_b:	defb    'a', 'b', 'c', 'd'
+main:
+	mov rsi, cpumsg
+	call output
+	xor eax, eax
+	mov rsi, 0x5012
+	lodsw
+	mov rdi, temp_string
+	mov rsi, rdi
+	call string_from_int
+	call output
 
-string_a:
-
-branches:
-		b		start			@ With comment!
-		bne     end
-		bl      linked_place
-
-		include test.s
-
-stack:
-stack_end:      defs	512			@ Stack Declaration
-
-		/* C Style comments
-		Can be multiline */
-start:
-		adr		r6, stack_end
-		mov		r0, #&3f
-		bic		r2, r0
-		mov		r1, sp
-		strb	r0, [r1, #stack]
-		nop
-		stmfd	sp!, {r0-r2, lr}
-		movs	r2, r2, lsr #1
-		cmp		r0, r2
-		addnes	r0, r0, #&1
-		bne		start
-		ldmfd	sp!, {r0-r2, pc}^
-		ldrbeq	x16, r1, [r2, #0x80]
-		pacdzb	x16
-		retab
-
-end:
-		svc		0
-		b 		{pc}+4, 
-
-.thumb
-		add 	r0, r1
+cpumsg:			db '[cpu: ', 0
